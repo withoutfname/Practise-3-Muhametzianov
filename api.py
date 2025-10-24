32c627074a78f7edcbc8c8f5294a78773247ec18
Эмиль Мухаметзянов <emilmuha123@gmail.com>
2025-10-24 10:29:39 +0500

    add main to api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from todo import todo_router
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +9,6 @@ async def welcome() -> dict:
     return {"message": "Привет Эмиль"}
 
 app.include_router(todo_router)
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
