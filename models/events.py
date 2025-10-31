c634dd2625adec2653ceb55192bd3d786b451db7
Эмиль Мухаметзянов <emilmuha123@gmail.com>
2025-10-31 10:38:24 +0500

    4 practise

---
+++ models/events.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+
+class Event(BaseModel):
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: List[str]
+    location: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book...",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
